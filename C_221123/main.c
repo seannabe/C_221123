425c68aaabaff139c8f96b2a2fe05be5efa9510d
seannabe <115057870+seannabe@users.noreply.github.com>
2022-11-23 11:50:47 +0800

    C_221123

---
+++ C_221123/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//int add(int x, int y);1111111111111111111111
+
+int main(void) {
+	
+	
+	/*2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2  
+	unsigned int i;
+	unsigned int seed;
+	
+	printf("%s", "Enter seed: ");
+	scanf("%u", &seed);
+	
+	srand (seed);
+	
+	for (i = 1; i <= 10; ++i) {
+
+		printf("%10d", 1 + (rand() % 6));
+
+		if (i % 5 == 0) {
+			puts(" ");
+		} 
+	}
+*/
+
+
+
+	/*111111111111111111111111111111111
+	int a = 7;
+	int b = 6;
+	int z = add(a, b);
+	printf("%d", z);
+*/
+	return 0;
+}
+
+
+/*1111111111111111111111
+int add(int x, int y) {
+	return x%y;
+}*/
